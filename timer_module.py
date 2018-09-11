57249191055bc8dabc9b6bf8afb9926daa07de85
倪彬辉 <binhui.ni@yourdream.cc>
2018-09-11 16:51:33 +0800

    添加了计时器模块

---
+++ timer_module.py
@@ -0,0 +1,50 @@
+'''
+计时器模块
+'''
+
+import time
+
+
+class Timer:
+    '''
+    计时器
+    '''
+
+    def __init__(self, func=time.perf_counter):
+        self.elapsed = 0.0
+        self._func = func
+        self._start = None
+
+    def start(self):
+        if self._start is not None:
+            raise RuntimeError('Already started')
+        self._start = self._func()
+
+    def stop(self):
+        if self._start is None:
+            raise RuntimeError('Not started')
+        end = self._func()
+        self.elapsed += end - self._start
+        self._start = None
+
+    def reset(self):
+        self.elapsed = 0.0
+
+    @property
+    def running(self):
+        return self._start is not None
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
+
+'''
+使用示例：
+t = Timer()
+do something.....
+t.stop()
+whole_time = t.elapsed
+'''
