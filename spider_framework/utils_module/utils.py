aace48c2ecdd371d2747a1a027c13ac37661545b
倪彬辉 <binhui.ni@yourdream.cc>
2018-10-17 15:50:25 +0800

    添加了utils工具类

---
+++ spider_framework/utils_module/utils.py
@@ -0,0 +1,114 @@
+from .log_module import get_logger, save_file_logger
+from .date import Date
+from ..config import *
+
+import requests
+import json
+import socket
+import os
+
+
+class Utility:
+    """
+    这是一个工具库，集成平时会复用的方法
+    """
+
+    def __init__(self):
+        self.logger = get_logger()
+
+    def get_cookies_from_pool(self, website, username):
+        '''
+        这个方法是用来从cookie池获取cookies绕过登录
+        '''
+        self.logger.info('获取cookies')
+        req = requests.get('http://10.0.5.58:7901/cookies?website={}&username={}'.format(website, username)).text
+        # req = requests.get('http://localhost:4500/cookies?website={}&username={}'.format(website, username)).text
+        cookies = json.loads(req)['data']
+
+        # 获取cookies
+        mycookies = {}
+        cookie_str = ''
+        if cookies:
+            try:
+                self.logger.info("拿到了账户{}的cookies".format(username))
+                for cookie in cookies:
+                    mycookies[cookie['name']] = cookie['value']
+                for c in mycookies:
+                    cookie_str += "{}={};".format(c, mycookies[c])
+                self.logger.info("cookie_str: {}".format(cookie_str))
+            except Exception as e:
+                self.logger.info('获取到了 str 类型的 cookies: {}'.format(e))
+                return cookies
+        else:
+            self.logger.info("没有账户{}的cookies".format(username))
+            cookie_str = ''
+        return cookie_str
+
+    def deploy_logs(self, project_name, whole_time, error_details, status_code, log_name='', **kwargs):
+
+        # 防止不存在输出日志文件
+        file_path = FILE_PATH
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        now = Date.now().format_es_utc_with_tz()
+        host_ip = self.get_host_ip()
+        if host_ip == '10.0.1.126':
+            machine = 'apitest'
+        elif host_ip == '10.0.5.58':
+            machine = 'service1'
+        elif host_ip == '10.0.5.59':
+            machine = 'service2'
+        elif host_ip == '192.168.8.117':
+            machine = 'localhost'
+        else:
+            machine = 'other_mechine'
+        self.logger.info("本机名称：{}".format(machine))
+        js_data = {
+            'date': now,
+            'machine': machine,
+            'project_name': project_name,
+            'whole_response_time': whole_time,
+            'error_details': error_details,
+            'status_code': status_code,  # int ,默认成功是0，其他的都是失败
+        }
+        for k, v in kwargs.items():
+            js_data[k] = v
+
+        js_data = json.dumps(js_data)
+        insert_data = js_data
+
+        # 写入到日志
+        today = Date.now().format(full=False)
+        if log_name:
+            self.logger.info('有log name: {}'.format(log_name))
+            save_logger = save_file_logger(file_path + '/{}.stdout.{}.log'.format(log_name, today))
+        else:
+            save_logger = save_file_logger(file_path + '/crawlers.stdout.{}.log'.format(today))
+        self.logger.info("开始写入日志")
+        save_logger.info(insert_data)
+        self.logger.info("写入日志完成")
+
+    def get_host_ip(self):
+        '''
+        获取本机IP
+        :return:
+        '''
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+        except Exception as e:
+            self.logger.error("get host ip error:{}".format(e))
+            ip = ''
+        finally:
+            s.close()
+            return ip
+
+    def xstr(self, s):
+        '''
+        用来把 s 转为字符串，如果 s 为0， 则返回 ''
+        '''
+        if s is None:
+            return ''
+        return str(s)
