c51d3b2ed12bbe011590bd964086ecbbf5051684
倪彬辉 <binhui.ni@yourdream.cc>
2018-10-17 18:10:38 +0800

    更新了配置文件

--- spider_framework/config.py
+++ spider_framework/config.py
@@ -8,9 +8,29 @@ MY_DEBUG_LEVEL = 'debug'
 # MYSQL config
 MYSQL_DB_NAME = 'checkin'
 MYSQL_HOST = '127.0.0.1'
-MYSQL_PORT = 6379
+MYSQL_PORT = 3306
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = ''
 MYSQL_CHARSET = 'utf8mb4'
 
-ES_ADDRESS = ''
+# -----------------Redis config-----------------
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB_ID = 0
+
+# -----------------api 设置-----------------
+# 获取内容失败
+FAILED_CODE = 400
+# 获取内容成功
+SUCCESS_CODE = 200
+# api debug模式默认开启
+DEBUG_STATUS = True
+# api 地址
+API_HOST = 'localhost'
+API_PORT = 4500
+
+# 日志保存路径
+FILE_PATH = '/Users/weasny/company/logs'
+
+# es 地址
+ES_ADDRESS = 'http://.......'
